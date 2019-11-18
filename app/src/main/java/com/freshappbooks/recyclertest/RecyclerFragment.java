a828099b095934f72349ddd43c051565e039602a
vjmarcus <vjmarcus1@gmail.com>
2019-11-18 19:54:04 +0300

    init commit

---
+++ app/src/main/java/com/freshappbooks/recyclertest/RecyclerFragment.java
@@ -0,0 +1,20 @@
+package com.freshappbooks.recyclertest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class RecyclerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_recycler, container, false);
+    }
+}
+
